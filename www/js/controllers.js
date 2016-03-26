6e7090877c2337d1a8d987a36d6a944e565bf0b7
Francis Clavette <clavette.francis@gmail.com>
2016-03-26 13:17:24 -0400

    added popup to ask user if he wants to share the sources despite of the errors in them

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -175,24 +175,39 @@ angular.module('starter.controllers', [])
     });
 
     $scope.share = function () {
-        var textToShare = "";
+        var textToShare = "Voici les sources du projet « " + $scope.project.name + " » : <br><br>";
+        var errNum = 0;
         for (var i = 0; i < $scope.project.sources.length; i++) {
-            textToShare += $scope.project.sources[i].parsedSource + "\n";
+            textToShare += $scope.project.sources[i].parsedSource + "<br><br>";
+            errNum += $scope.project.sources[i].errors.length;
         }
-        window.plugins.socialsharing.shareViaEmail(
-          textToShare,
-          project.name,
-          [], // TO: must be null or an array
-          [], // CC: must be null or an array
-          null, // BCC: must be null or an array
-          [], // FILES: can be null, a string, or an array
-          function () { // Success
 
-          },
-          function () { // Error
-
-          }
-        );
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Erreur',
+            template: '<p class="center">Les sources que vous essayez de partager contiennent <strong>' + errNum + '</strong> erreur(s). Voulez-vous les partager quand même?</p>',
+            cancelText: 'Annuler',
+            okText: '<b>Partager</b>'
+        });
+        confirmPopup.then(function(res) {
+            if(res) {
+                window.plugins.socialsharing.shareViaEmail(
+                  textToShare,
+                  $scope.project.name,
+                  [], // TO: must be null or an array
+                  [], // CC: must be null or an array
+                  null, // BCC: must be null or an array
+                  [], // FILES: can be null, a string, or an array
+                  function () { // Success
+                      console.log("success");
+                  },
+                  function () { // Error
+                      console.log("error");
+                  }
+                );
+            } else {
+                console.log("Cancelled by user");
+            }
+        });
     }
 
     $scope.resetModalVars = function () {
