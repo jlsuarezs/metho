6fb078fe64bc5e37237fe250b92f68273b603b01
Francis Clavette <clavette.francis@gmail.com>
2016-03-31 19:13:24 -0400

    added condition for cordova init in app.js for error not to show

--- www/js/app.js
+++ www/js/app.js
@@ -10,7 +10,10 @@ angular.module('metho', ['ionic', 'metho.controllers.projects', 'metho.controlle
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    cordova.plugins.Keyboard.disableScroll(false);
+    if (!!window.cordova) {
+      cordova.plugins.Keyboard.disableScroll(false);
+    }
+
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
