0ba4c61d377fd13e93d511dedff7a195e630fb0c
Francis Clavette <clavette.francis@gmail.com>
2016-04-08 21:14:17 -0400

    beautify html files

--- gulpfile.js
+++ gulpfile.js
@@ -7,33 +7,36 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
 var beautify = require('gulp-jsbeautify');
+var prettify = require('gulp-prettify');
 
 var paths = {
   sass: ['./scss/**/*.scss'],
   js: ['./www/js/*.js'],
-  css: ['./www/css/*.css']
+  css: ['./www/css/*.css'],
+  html: ['./www/templates/*.html']
 };
 
-gulp.task('default', ['js']);
+gulp.task('default', ['beautifyjs', 'beautifyhtml']);
 
-gulp.task('js', function() {
+gulp.task('beautifyjs', function() {
   gulp.src('./www/js/*.js')
-    .pipe(beautify({indentSize: 4, maxPreserveNewline: 2, breakChainedOperation: true}))
+    .pipe(beautify({indentSize: 4, maxPreserveNewline: 2, breakChainedOperation: true, endWithNewline: true}))
     .pipe(gulp.dest('./www/js/'));
 });
 
+gulp.task('beautifyhtml', function() {
+  gulp.src('./www/templates/*.html')
+    .pipe(prettify({indent_size: 4}))
+    .pipe(gulp.dest('./www/templates/'));
+});
+
 gulp.task('watch', function() {
-  gulp.watch(paths.js, ['js']);
+  gulp.watch(paths.js, ['beautifyjs']);
+  gulp.watch(paths.css, ['minifycss']);
+  gulp.watch(paths.html, ['beautifyhtml']);
 });
 
-gulp.task('install', ['git-check'], function() {
-    return bower.commands.install()
-    .on('log', function(data) {
-        gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('css', function () {
+gulp.task('minifycss', function () {
     gulp.src('./www/css/*.css')
         .pipe(minifyCss())
         .pipe(rename(function (path) {
