e51024c725b32d471d66b2f814cee56d07f15b33
Francis Clavette <clavette.francis@gmail.com>
2016-04-08 20:57:12 -0400

    added task for minifying CSS

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@ var beautify = require('gulp-jsbeautify');
 
 var paths = {
   sass: ['./scss/**/*.scss'],
-  js: ['./www/js/*.js']
+  js: ['./www/js/*.js'],
+  css: ['./www/css/*.css']
 };
 
 gulp.task('default', ['js']);
@@ -26,21 +27,18 @@ gulp.task('watch', function() {
 });
 
 gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
+    return bower.commands.install()
     .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+        gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+gulp.task('css', function () {
+    gulp.src('./www/css/*.css')
+        .pipe(minifyCss())
+        .pipe(rename(function (path) {
+            path.extname = ".min.css";
+            return path;
+        }))
+        .pipe(gulp.dest('./www/css/'));
 });
