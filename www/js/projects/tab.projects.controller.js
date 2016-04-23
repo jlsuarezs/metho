1451b193f6737413a4f81c3c23c93b81eb9f06ff
Francis Clavette <clavette.francis@gmail.com>
2016-04-23 15:14:48 -0400

    fixed bug where projects would be loaded twice

--- www/js/projects/tab.projects.controller.js
+++ www/js/projects/tab.projects.controller.js
@@ -41,8 +41,6 @@ angular.module('metho.controller.projects.tab', [])
         });
     }
 
-    $scope.loadProjects();
-
     $rootScope.$on("$translateChangeSuccess", function () {
         $scope.loadProjects();
     });
