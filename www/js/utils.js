3f185d4a0ec51eecbe5ed2108705db1089fbdf0a
Francis Clavette <clavette.francis@gmail.com>
2016-05-05 20:13:53 -0400

    added prototype func to Array to convert Objects to Array

--- www/js/utils.js
+++ www/js/utils.js
@@ -1,3 +1,11 @@
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
+
+Array.prototype.fromObject = function (obj) {
+    var ar = [];
+    for(item in obj){
+        ar.push(obj[item]);
+    }
+    return ar;
+};
