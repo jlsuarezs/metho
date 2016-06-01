d4100b3ac90e457f1bd818dbadc77899e139d93b
Francis Clavette <clavette.francis@gmail.com>
2016-05-31 21:19:16 -0400

    Migrated project-modal

--- app/pages/project-modal/project-modal.ts
+++ app/pages/project-modal/project-modal.ts
@@ -1,14 +1,56 @@
-import {Page, NavController} from 'ionic-angular';
+import {Page, NavController, ViewController, NavParams} from 'ionic-angular';
+import {TranslatePipe} from 'ng2-translate/ng2-translate';
+import {FormBuilder, Validators, ControlGroup} from '@angular/common';
 
-/*
-  Generated class for the ProjectModalPage page.
+import {AppStorage} from '../../providers/app-storage/app-storage.ts';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Page({
   templateUrl: 'build/pages/project-modal/project-modal.html',
+  pipes: [TranslatePipe]
 })
 export class ProjectModalPage {
-  constructor(public nav: NavController) {}
+  public viewCtrl: ViewController;
+  public navParams: NavParams;
+  public isNew: Boolean;
+  public previous: any;
+  public projectForm: ControlGroup;
+  private storage: AppStorage;
+
+  constructor(viewCtrl: ViewController, navParams: NavParams, fb: FormBuilder, storage: AppStorage) {
+    this.viewCtrl = viewCtrl;
+    this.navParams = navParams;
+    this.storage = storage;
+    if(this.navParams.get('previous')) {
+      this.isNew = false;
+      this.previous = this.navParams.get('previous');
+    }else {
+      this.isNew = true;
+      this.previous = {
+        name: "",
+        matter: ""
+      };
+    }
+    this.projectForm = fb.group({
+      name: [this.previous.name, Validators.required],
+      matter: [this.previous.matter, Validators.nullValidator]
+    });
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  confirm() {
+    if(this.projectForm.valid) {
+      if(this.isNew) {
+        this.storage.createProject(this.projectForm.value).then(() => {
+          this.viewCtrl.dismiss();
+        });
+      }else {
+        this.storage.setProjectFromId(this.previous._id, this.projectForm.value).then(() => {
+          this.viewCtrl.dismiss();
+        });
+      }
+    }
+  }
 }
