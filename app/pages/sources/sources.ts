7df3fdf8dca151d644241c72472a2342a91b38a9
Francis Clavette <clavette.francis@gmail.com>
2016-06-01 20:22:50 -0400

    Migrated Sources view (formerly Source-Detail)

--- app/pages/sources/sources.ts
+++ app/pages/sources/sources.ts
@@ -1,14 +1,183 @@
-import {Page, NavController} from 'ionic-angular';
+import {Page, NavController, NavParams, ActionSheet, Modal, Alert, List} from 'ionic-angular';
+import {ViewChild} from '@angular/core';
+import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
 
-/*
-  Generated class for the SourcesPage page.
+import {SourcePage} from '../source/source';
+import {SourceModalPage} from '../source-modal/source-modal';
+import {PendingsPage} from '../pendings/pendings';
+import {AppStorage} from '../../providers/app-storage/app-storage';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Page({
   templateUrl: 'build/pages/sources/sources.html',
+  pipes: [TranslatePipe]
 })
 export class SourcesPage {
-  constructor(public nav: NavController) {}
+  public projectId: string;
+  public sources: Array<any> = [];
+  public project: any;
+  public pendingNumber: number;
+  private currentTransition: any;
+  @ViewChild(List) list: List;
+
+  constructor(public nav: NavController, public params: NavParams, public translate: TranslateService, public storage: AppStorage) {
+    this.projectId = params.get('id');
+    this.loadSources();
+    this.loadProjectInfo();
+    this.loadPendingNumber();
+  }
+
+  loadSources() {
+    this.storage.getSourcesFromProjectId(this.projectId).then(sources => {
+      this.sources = sources;
+      this.sources.sort(function(a, b) {
+        if (a.title && b.title) {
+          return a.title.localeCompare(b.title);
+        } else if (a.title) {
+          return a.title.localeCompare(b.parsedSource);
+        } else if (b.title) {
+          return a.parsedSource.localeCompare(b.title);
+        }
+      });
+    });
+  }
+
+  loadProjectInfo() {
+    this.storage.getProjectFromId(this.projectId).then(project => {
+      this.project = project;
+    });
+  }
+
+  loadPendingNumber() {
+    this.storage.getPendingNumber(this.projectId).then(num => {
+      this.pendingNumber = num;
+    });
+  }
+
+  createSource() {
+    this.translate.get(["PROJECT.TYPES.BOOK", "PROJECT.TYPES.ARTICLE", "PROJECT.TYPES.INTERNET", "PROJECT.TYPES.CD", "PROJECT.TYPES.MOVIE", "PROJECT.TYPES.INTERVIEW", "PROJECT.DETAIL.CHOOSE_TYPE", "PROJECT.DETAIL.POPUP.CANCEL"]).subscribe(translations => {
+      let action = ActionSheet.create({
+        title: translations['PROJECT.DETAIL.CHOOSE_TYPE'],
+        buttons: [
+          {
+            text: translations['PROJECT.TYPES.BOOK'],
+            handler: () => {
+              this.currentTransition = action.dismiss();
+              this.openModal('book', null);
+              return false;
+            }
+          },
+          {
+            text: translations['PROJECT.TYPES.ARTICLE'],
+            handler: () => {
+              this.currentTransition = action.dismiss();
+              this.openModal('article', null);
+              return false;
+            }
+          },
+          {
+            text: translations['PROJECT.TYPES.INTERNET'],
+            handler: () => {
+              this.currentTransition = action.dismiss();
+              this.openModal('internet', null);
+              return false;
+            }
+          },
+          {
+            text: translations['PROJECT.TYPES.CD'],
+            handler: () => {
+              this.currentTransition = action.dismiss();
+              this.openModal('cd', null);
+              return false;
+            }
+          },
+          {
+            text: translations['PROJECT.TYPES.MOVIE'],
+            handler: () => {
+              this.currentTransition = action.dismiss();
+              this.openModal('movie', null);
+              return false;
+            }
+          },
+          {
+            text: translations['PROJECT.TYPES.INTERVIEW'],
+            handler: () => {
+              this.currentTransition = action.dismiss();
+              this.openModal('interview', null);
+              return false;
+            }
+          },
+          {
+            role: 'cancel',
+            text: translations['PROJECT.DETAIL.POPUP.CANCEL']
+          }
+        ]
+      });
+
+      this.nav.present(action);
+    });
+  }
+
+  openModal(type: string, existing: any) {
+    let modal = Modal.create(SourceModalPage, {
+      type: type,
+      edit: existing ? existing : false
+    });
+
+    modal.onDismiss(() => {
+      this.loadSources();
+      this.loadPendingNumber();
+    });
+
+    if(this.currentTransition) {
+      this.currentTransition.then(() => {
+        this.currentTransition = null;
+        this.nav.present(modal);
+      });
+    }else {
+      this.nav.present(modal);
+    }
+  }
+
+  deleteSource(source: any) {
+    this.translate.get(["PROJECT.DETAIL.POPUP.DELETE_TITLE", "PROJECT.DETAIL.POPUP.DELETE_TEXT", "PROJECT.DETAIL.POPUP.DELETE", "PROJECT.DETAIL.POPUP.CANCEL"]).subscribe(translations => {
+      let alert = Alert.create({
+        title: translations["PROJECT.DETAIL.POPUP.DELETE_TITLE"],
+        message: translations["PROJECT.DETAIL.POPUP.DELETE_TEXT"],
+        buttons: [
+          {
+            text: translations["PROJECT.DETAIL.POPUP.CANCEL"],
+            handler: () => {
+              this.list.closeSlidingItems();
+            }
+          },
+          {
+            text: translations["PROJECT.DETAIL.POPUP.DELETE"],
+            handler: () => {
+              this.storage.deleteSource(source._id);
+              this.sources.splice(this.sources.indexOf(source), 1);
+            }
+          }
+        ]
+      });
+
+      this.nav.present(alert);
+    });
+  }
+
+  share() {
+
+  }
+
+  openSourcePage(source: any) {
+    this.nav.push(SourcePage, {
+      pId: this.projectId,
+      id: source._id
+    });
+  }
+
+  openPendingPage() {
+    this.nav.push(PendingsPage, {
+      pId: this.projectId
+    });
+  }
 }
