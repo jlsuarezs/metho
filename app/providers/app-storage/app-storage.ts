d1a5e6ed27d87895465d53011539dd132ecdaed8
Francis Clavette <clavette.francis@gmail.com>
2016-06-01 20:21:12 -0400

    Migrated AppStorage provider (formerly Storage service)

--- app/providers/app-storage/app-storage.ts
+++ app/providers/app-storage/app-storage.ts
@@ -1,25 +1,26 @@
 import {Injectable, EventEmitter} from '@angular/core';
 import {Storage, LocalStorage} from 'ionic-angular';
-import 'rxjs/add/operator/map';
+import {Parse} from '../parse/parse';
+import {ReportUser} from '../user-report/user-report';
 
 var PouchDB = require("pouchdb");
 
 @Injectable()
 export class AppStorage {
-  theresProjects: Boolean = false;
-  projectDB: Object = null;
-  sourceDB: Object = null;
-  pendingDB: Object = null;
-  local: any;
-  projects: Object = {};
-  sources: Object = {};
-  sourcesByProject: Object = {};
-  pendings: Object = {};
-  pendingsByProject: Object = {};
+  private theresProjects: Boolean = false;
+  private projectDB: any = null;
+  private sourceDB: any = null;
+  private pendingDB: any = null;
+  private local: any;
+  private projects: Object = {};
+  private sources: Object = {};
+  private sourcesByProject: Object = {};
+  private pendings: Object = {};
+  private pendingsByProject: Object = {};
 
-  loadingProjects: Boolean = true;
-  loadingSources: Boolean = true;
-  loadingPendings: Boolean = true;
+  private loadingProjects: Boolean = true;
+  private loadingSources: Boolean = true;
+  private loadingPendings: Boolean = true;
 
   private projectEvents = new EventEmitter();
   private sourcesEvents = new EventEmitter();
@@ -102,71 +103,290 @@ export class AppStorage {
   }
 
   deleteProject(id: string) {
+    var doc = this.projects[id];
+    delete this.projects[id];
+    var arr_sourcesToDelete: any = [];
+    for (var i = 0; i < this.sourcesByProject[id].length; i++) {
+      delete this.sources[this.sourcesByProject[id][i]._id];
+      arr_sourcesToDelete.push(this.sourcesByProject[id][i]);
+    }
+    delete this.sourcesByProject[id];
 
+
+    for (var i = 0; i < arr_sourcesToDelete.length; i++) {
+      this.sourceDB.remove(arr_sourcesToDelete[i]);
+    }
+
+    if (this.fromObject(this.projects).length == 0) {
+      this.local.set("theresProjects", false);
+    }
+
+    return new Promise(resolve => {
+      this.projectDB.remove(doc).then(result => {
+        resolve(result);
+      }).catch(err => {
+        // ReportUser.report(err);
+        resolve(err);
+      });
+    });
   }
 
-  setProjectFromId(id: string, set: Object) {
-
+  setProjectFromId(id: string, set: any) {
+    return new Promise(resolve => {
+      this.projectDB.put(set, id, this.projects[id]._rev).then(response => {
+        set._rev = response.rev;
+        this.projects[id] = set;
+        resolve(response);
+      }).catch(err =>{
+        // ReportUser.report(err);
+        resolve(err);
+      });
+    });
   }
 
   getProjectFromId(id: string) {
-
+    if(this.loadingProjects){
+      return new Promise(resolve => {
+        this.projectEvents.subscribe(event => {
+          resolve(this.projects[id]);
+        });
+      });
+    }else {
+      return Promise.resolve(this.projects[id]);
+    }
   }
 
-  createProject(project: Object) {
-
+  createProject(project: any) {
+    this.loadingProjects = true;
+    this.loadingSources = true;
+    return new Promise(resolve => {
+      this.projectDB.post(project).then(response => {
+        this.sourcesByProject[response.id] = {};
+        project._id = response.id;
+        project._rev = response.rev;
+        this.projects[response.id] = project;
+        this.loadingProjects = false;
+        this.loadingSources = false;
+        this.projectEvents.emit("projectLoadingEnded");
+        this.sourcesEvents.emit("sourceLoadingEnded");
+        this.local.set("theresProjects", true);
+        resolve(response);
+      }).catch(function(err) {
+        this.loadingProjects = false;
+        this.loadingSources = false;
+        this.projectEvents.emit("projectLoadingEnded");
+        this.sourcesEvents.emit("sourceLoadingEnded");
+        this.local.set("theresProjects", true);
+        // ReportUser.report(err);
+        resolve(err);
+      });
+    });
   }
 
   getSourcesFromProjectId(id: string) {
-
+    if(this.loadingSources){
+      return new Promise(resolve => {
+        this.sourcesEvents.subscribe(event => {
+          resolve(this.fromObject(this.sourcesByProject[id]));
+        });
+      });
+    }else {
+      return Promise.resolve(this.fromObject(this.sourcesByProject[id]));
+    }
   }
 
   getSourcesFromId(id: string) {
-
+    if(this.loadingSources){
+      return new Promise(resolve => {
+        this.sourcesEvents.subscribe(event => {
+          resolve(this.sources[id]);
+        });
+      });
+    }else {
+      return Promise.resolve(this.sources[id]);
+    }
   }
 
-  setSourceFromId(id: string, set: Object) {
-
+  setSourceFromId(id: string, set: any) {
+    return new Promise(resolve => {
+      this.sourceDB.put(set, id, this.sources[id]._rev).then(response => {
+        set._rev = response.rev;
+        this.sources[id] = set;
+        resolve(response);
+      }).catch(err => {
+        // ReportUser.report(err);
+        resolve(err);
+      });
+    });
   }
 
-  createSource(source: Object) {
-
+  createSource(source: any) {
+    this.loadingSources = true;
+    return new Promise(resolve => {
+      this.sourceDB.post(source).then(response => {
+        source._id = response.id;
+        source._rev = response.rev;
+        this.sources[response.id] = source;
+        this.sourcesByProject[source.project_id][response.id] = source;
+        this.loadingSources = false;
+        this.sourcesEvents.emit("sourceLoadingEnded");
+        resolve(response);
+      }).catch(function(err) {
+        this.loadingSources = false;
+        this.sourcesEvents.emit("sourceLoadingEnded");
+        // ReportUser.report(err);
+        resolve(err);
+      });
+    });
   }
 
   deleteSource(id: string) {
+    let doc = this.sources[id];
+    delete this.sources[id];
+    delete this.sourcesByProject[doc.project_id][id];
 
+    return new Promise(resolve => {
+      this.sourceDB.remove(doc).then(result => {
+        resolve(result);
+      }).catch(err => {
+        // ReportUser.report(err);
+        resolve(err);
+      });
+    });
   }
 
   parseSources() {
-
-  }
-
-  getPendings() {
-    // Deprecated
+    if(this.loadingSources) {
+      return new Promise(resolve => {
+        this.sourcesEvents.subscribe(() => {
+          this.loadingSources = true;
+          let arrSources = this.fromObject(this.sources);
+          let source: any = {};
+          for (var i = 0; i < arrSources.length; i++) {
+            source[arrSources[i]._id] = Parse.parse(arrSources[i]);
+            if(i == arrSources.length - 1) {
+              this.sourceDB.put(source[arrSources[i]._id]).then(response => {
+                source[response.id]._rev = response.rev;
+                this.sources[response.id] = source[response.id];
+                this.sourcesByProject[source[response.id].project_id][response.id] = source[response.id];
+                this.loadingSources = false;
+                this.sourcesEvents.emit("sourceLoadingEnded");
+                resolve(true);
+              });
+            }else {
+              this.sourceDB.put(source[arrSources[i]._id]).then(response => {
+                source[response.id]._rev = response.rev;
+                this.sources[response.id] = source[response.id];
+                this.sourcesByProject[source[response.id].project_id][response.id] = source[response.id];
+              });
+            }
+          }
+        });
+      });
+    }else {
+      return new Promise(resolve => {
+        this.loadingSources = true;
+        let arrSources = this.fromObject(this.sources);
+        let source = {};
+        for (var i = 0; i < arrSources.length; i++) {
+          source[arrSources[i]._id] = Parse.parse(arrSources[i]);
+          if(i == arrSources.length - 1) {
+            this.sourceDB.put(source[arrSources[i]._id]).then(response => {
+              source[response.id]._rev = response.rev;
+              this.sources[response.id] = source[response.id];
+              this.sourcesByProject[source[response.id].project_id][response.id] = source[response.id];
+              this.loadingSources = false;
+              this.sourcesEvents.emit("sourceLoadingEnded");
+              resolve(true);
+            });
+          }else {
+            this.sourceDB.put(source[arrSources[i]._id]).then(response => {
+              source[response.id]._rev = response.rev;
+              this.sources[response.id] = source[response.id];
+              this.sourcesByProject[source[response.id].project_id][response.id] = source[response.id];
+            });
+          }
+        }
+      });
+    }
   }
 
   getPendingsFromProjectId(id: string) {
-
+    if(this.loadingPendings){
+      return new Promise(resolve => {
+        this.pendingsEvents.subscribe(event => {
+          resolve(this.fromObject(this.pendingsByProject[id]));
+        });
+      });
+    }else {
+      return Promise.resolve(this.fromObject(this.pendingsByProject[id]));
+    }
   }
 
-  createPending(pending: Object) {
-
+  createPending(pending: any) {
+    this.loadingPendings = true;
+    return new Promise(resolve => {
+      this.pendingDB.post(pending).then(response => {
+        pending._id = response.id;
+        pending._rev = response.rev;
+        this.pendings[response.id] = pending;
+        this.pendingsByProject[pending.project_id][response.id] = pending;
+        this.loadingPendings = false;
+        this.pendingsEvents.emit("pendingLoadingEnded");
+        resolve(response);
+      }).catch(function(err) {
+        this.loadingPendings = false;
+        this.pendingsEvents.emit("pendingLoadingEnded");
+        // ReportUser.report(err);
+        resolve(err);
+      });
+    });
   }
 
   deletePending(id: string) {
+    let doc = this.pendings[id];
+    delete this.pendings[id];
+    delete this.pendingsByProject[doc.project_id][id];
 
+    return new Promise(resolve => {
+      this.pendingDB.remove(doc).then(result => {
+        resolve(result);
+      }).catch(err => {
+        // ReportUser.report(err);
+        resolve(err);
+      });
+    });
   }
 
-  setPendingFromId(id: string, set: Object) {
-
+  setPendingFromId(id: string, set: any) {
+    return new Promise(resolve => {
+      this.pendingDB.put(set, id, this.pendings[id]._rev).then(response => {
+        set._rev = response.rev;
+        this.pendings[id] = set;
+        resolve(response);
+      }).catch(err => {
+        // ReportUser.report(err);
+        resolve(err);
+      });
+    });
   }
 
   getPendingNumber(id: string) {
-
+    if (this.loadingPendings) {
+      return new Promise(resolve => {
+        this.pendingsEvents.subscribe(event => {
+          let arr_pendings = this.fromObject(this.pendingsByProject[id]);
+          resolve(arr_pendings.length ? arr_pendings.length : 0);
+        });
+      });
+    }else {
+      let arr_pendings = this.fromObject(this.pendingsByProject[id]);
+      return Promise.resolve(arr_pendings.length ? arr_pendings.length : 0);
+    }
   }
 
   fromObject(obj: Object) {
-    var ar = [];
+    var ar: Array<Object> = [];
     for(var item in obj){
         ar.push(obj[item]);
     }
