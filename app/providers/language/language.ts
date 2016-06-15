7a517c325468be911dbce5509f17ff4b69c6266a
Francis Clavette <clavette.francis@gmail.com>
2016-06-15 17:46:16 -0400

    Fixed bugs and added current() method

--- app/providers/language/language.ts
+++ app/providers/language/language.ts
@@ -1,18 +1,21 @@
 import {Injectable} from '@angular/core';
 import {TranslateService} from 'ng2-translate/ng2-translate';
+import {Globalization} from 'ionic-native';
 import * as moment from 'moment';
 import 'moment/locale/fr';
 import 'moment/locale/en-ca';
 import 'moment/locale/es';
-var numeral = require('numeral');
-import 'numeral/min/languages.min';
+// var numeral = require('numeral');
+// import 'numeral/languages/fr';
+// import 'numeral/languages/en-gb';
+// import 'numeral/languages/es';
 
 import {Settings} from '../settings/settings';
 
-import {Globalization} from 'ionic-native';
 
 @Injectable()
 export class Language {
+  public currentLang: string;
 
   constructor(public settings: Settings, public translate: TranslateService) {}
 
@@ -20,14 +23,22 @@ export class Language {
     this.translate.setDefaultLang('en');
     if (this.settings.get('overideLang') == "") {
       Globalization.getPreferredLanguage().then(lang => {
-        this.translate.use(lang.value);
-        moment.locale(lang.value);
-        numeral.language(lang.value);
+        let code = lang.value.split("-")[0];
+        this.translate.use(code);
+        moment.locale(code);
+        this.currentLang = code;
+        // numeral.language(code);
+      }).catch(err => {
+        this.translate.use("fr");
+        moment.locale("fr");
+        this.currentLang = "fr";
+        // numeral.language("fr");
       });
     }else {
       this.translate.use(this.settings.get('overideLang'));
       moment.locale(this.settings.get('overideLang'));
-      numeral.language(this.settings.get('overideLang'));
+      this.currentLang = this.settings.get('overideLang');
+      // numeral.language(this.settings.get('overideLang'));
     }
   }
 
@@ -35,12 +46,16 @@ export class Language {
     return moment;
   }
 
-  getNumeral() {
-    return numeral;
-  }
+  // getNumeral() {
+  //   return numeral;
+  // }
 
   change(lang: string) {
     this.settings.set('overideLang', lang);
     this.init();
+  }
+
+  current(): string {
+    return this.currentLang;
   }
 }
