996588cf92ddb11912f79946b3feaa54c4bd7fdc
Francis Clavette <clavette.francis@gmail.com>
2016-06-15 17:50:55 -0400

    Migrated SettingsPage

--- app/pages/settings/settings.ts
+++ app/pages/settings/settings.ts
@@ -1,14 +1,102 @@
-import {Page, NavController} from 'ionic-angular';
+import {Page, NavController, Alert, List} from 'ionic-angular';
+import {ViewChild} from '@angular/core';
+import {TranslatePipe, TranslateService} from 'ng2-translate/ng2-translate';
 
-/*
-  Generated class for the SettingsPage page.
+import {AdvancedModePage} from '../advanced-mode/advanced-mode';
+import {AttributionsPage} from '../attributions/attributions';
+import {FeedbackPage} from '../feedback/feedback';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+import {Settings} from '../../providers/settings/settings';
+import {Language} from '../../providers/language/language';
+import {AdvancedMode} from '../../providers/advanced-mode/advanced-mode';
+
 @Page({
   templateUrl: 'build/pages/settings/settings.html',
+  pipes: [TranslatePipe]
 })
 export class SettingsPage {
-  constructor(public nav: NavController) {}
+  public settings: any = {};
+  public advancedPage: any;
+  public attributionsPage: any;
+  public feedbackPage: any;
+  @ViewChild(List) list: List;
+
+  constructor(public nav: NavController, public translate: TranslateService,  public settingService: Settings, public advanced: AdvancedMode, public language: Language) {
+    this.advancedPage = AdvancedModePage;
+    this.attributionsPage = AttributionsPage;
+    this.feedbackPage = FeedbackPage;
+    this.loadSettings();
+    console.log(this.settings);
+  }
+
+  loadSettings() {
+    this.settings = this.settingService.getAll();
+  }
+
+  ionViewWillEnter() {
+    this.loadSettings();
+  }
+
+  toggleAdvanced() {
+    if (this.settings.advanced == true) {
+      if (!this.advanced.enable()) {
+        this.settings.advanced = false;
+      }
+    }else {
+      this.advanced.disable();
+    }
+  }
+
+  changeOrder() {
+    this.settingService.set('defaultOrder', this.settings.defaultOrder);
+  }
+
+  editName() {
+    this.translate.get(["SETTINGS.EDIT_NAME", "SETTINGS.CANCEL", "SETTINGS.EDIT"]).subscribe(translations => {
+      let alert = Alert.create({
+        title: translations["SETTINGS.EDIT_NAME"],
+        inputs: [
+          {
+            type: "text",
+            name: "firstname",
+            value: this.settings.firstname
+          },
+          {
+            type: "text",
+            name: "lastname",
+            value: this.settings.lastname
+          }
+        ],
+        buttons: [
+          {
+            text: translations["SETTINGS.CANCEL"],
+            handler: () => {
+              this.list.closeSlidingItems();
+            }
+          },
+          {
+            text: translations["SETTINGS.EDIT"],
+            handler: results => {
+              this.settingService.set('firstname', results.firstname);
+              this.settingService.set('lastname', results.lastname);
+              this.loadSettings();
+              this.list.closeSlidingItems();
+            }
+          }
+        ]
+      });
+
+      this.nav.present(alert);
+    });
+  }
+
+  forgetName() {
+    this.settingService.set('firstname', '');
+    this.settingService.set('lastname', '');
+    this.loadSettings();
+  }
+
+  changeLanguage() {
+    this.language.change(this.settings.overideLang);
+  }
 }
