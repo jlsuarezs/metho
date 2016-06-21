6bee10a85c77b56120af3e10d3da0b8341352c74
Francis Clavette <clavette.francis@gmail.com>
2016-06-21 16:13:11 -0400

    Migrated FeedbackPage

--- app/pages/feedback/feedback.ts
+++ app/pages/feedback/feedback.ts
@@ -1,14 +1,105 @@
 import {Page, NavController} from 'ionic-angular';
 
-/*
-  Generated class for the FeedbackPage page.
+import {SocialSharing, Device} from 'ionic-native';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
+
 @Page({
   templateUrl: 'build/pages/feedback/feedback.html',
+  pipes: [TranslatePipe]
 })
 export class FeedbackPage {
-  constructor(public nav: NavController) {}
+  constructor(public nav: NavController, public translate: TranslateService) {}
+
+  open(type: string) {
+    switch (type) {
+      case "projects":
+        this.translate.get(['SETTINGS.FEEDBACK.EMAIL.PROJECT', 'SETTINGS.FEEDBACK.EMAIL.PROJECT_TITLE']).subscribe(translations => {
+          SocialSharing.shareViaEmail(
+            `${translations["SETTINGS.FEEDBACK.EMAIL.PROJECT"]}
+            ${Device.device.platform} ${Device.device.version}
+            <br>
+            ${Device.device.model}
+            <br>
+            ${window.screen.width * window.devicePixelRatio}x${window.screen.height * window.devicePixelRatio}
+            <br>
+            Cordova ${Device.device.cordova}
+            </p>
+            `,
+            translations["SETTINGS.FEEDBACK.EMAIL.PROJECT_TITLE"],
+            ['methoappeei@gmail.com'],
+            [],
+            [],
+            null
+          );
+        });
+        break;
+      case "reference":
+        this.translate.get(['SETTINGS.FEEDBACK.EMAIL.REFERENCE', 'SETTINGS.FEEDBACK.EMAIL.REFERENCE_TITLE']).subscribe(translations => {
+          SocialSharing.shareViaEmail(
+            `${translations['SETTINGS.FEEDBACK.EMAIL.REFERENCE']}
+            ${Device.device.platform} ${Device.device.version}
+            <br>
+            ${Device.device.model}
+            <br>
+            ${window.screen.width * window.devicePixelRatio}x${window.screen.height * window.devicePixelRatio}
+            <br>
+            Cordova ${Device.device.cordova}
+            </p>
+            `,
+            translations["SETTINGS.FEEDBACK.EMAIL.REFERENCE_TITLE"],
+            ['methoappeei@gmail.com'],
+            [],
+            [],
+            null
+          );
+        });
+        break;
+      case "settings":
+        this.translate.get(['SETTINGS.FEEDBACK.EMAIL.SETTINGS', 'SETTINGS.FEEDBACK.EMAIL.SETTINGS_TITLE']).subscribe(translations => {
+          SocialSharing.shareViaEmail(
+            `${translations["SETTINGS.FEEDBACK.EMAIL.SETTINGS"]}
+            ${Device.device.platform} ${Device.device.version}
+            <br>
+            ${Device.device.model}
+            <br>
+            ${window.screen.width * window.devicePixelRatio}x${window.screen.height * window.devicePixelRatio}
+            <br>
+            Cordova ${Device.device.cordova}
+            </p>
+            `,
+            translations["SETTINGS.FEEDBACK.EMAIL.SETTINGS_TITLE"],
+            ['methoappeei@gmail.com'],
+            [],
+            [],
+            null
+          );
+        });
+        break;
+      case "comment":
+        this.translate.get(['SETTINGS.FEEDBACK.EMAIL.COMMENT', 'SETTINGS.FEEDBACK.EMAIL.COMMENT_TITLE']).subscribe(translations => {
+          SocialSharing.shareViaEmail(
+            translations["SETTINGS.FEEDBACK.EMAIL.COMMENT"],
+            translations["SETTINGS.FEEDBACK.EMAIL.COMMENT_TITLE"],
+            ['methoappeei@gmail.com'],
+            [],
+            [],
+            null
+          );
+        });
+        break;
+      case "feature":
+        this.translate.get(['SETTINGS.FEEDBACK.EMAIL.FEATURE', 'SETTINGS.FEEDBACK.EMAIL.FEATURE_TITLE']).subscribe(translations => {
+          SocialSharing.shareViaEmail(
+            translations["SETTINGS.FEEDBACK.EMAIL.FEATURE"],
+            translations["SETTINGS.FEEDBACK.EMAIL.FEATURE_TITLE"],
+            ['methoappeei@gmail.com'],
+            [],
+            [],
+            null
+          );
+        });
+        break;
+    }
+  }
 }
