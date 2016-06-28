c6aec524e4603e9dcba471ac7b829bb357135b63
Francis Clavette <clavette.francis@gmail.com>
2016-06-28 15:12:06 -0400

    Debugged UserReport

--- app/providers/user-report/user-report.ts
+++ app/providers/user-report/user-report.ts
@@ -1,21 +1,31 @@
 import {Injectable} from '@angular/core';
 import {Alert, App, NavController} from 'ionic-angular';
-import {EmailComposer, Device} from 'ionic-native';
+import {SocialSharing, Device} from 'ionic-native';
 import {TranslateService} from 'ng2-translate/ng2-translate';
 
 @Injectable()
 export class UserReport {
   public nav: NavController;
 
-  constructor(public translate: TranslateService, public app: App) {
-    this.nav = this.app.getActiveNav();
-  }
+  constructor(public translate: TranslateService, public app: App) {}
 
   report(err: any) {
+    console.log(err);
+    let errStr: string = err;
+    let stacktrace: string = "";
     if (typeof err != 'string') {
-      err = JSON.parse(err);
+      errStr = err.toString();
     }
-    this.translate.get(["YES", "NO", "REPORT.ERROR", "REPORT.UNKNOWN", "REPORT.REPORT_?","REPORT.DO_NOT_EDIT", "REPORT.TITLE"]).subscribe(translations => {
+
+    if (typeof err == 'object') {
+      try {
+        stacktrace = err.stack;
+      } catch (e) {
+        console.log(e);
+        stacktrace = "";
+      }
+    }
+    this.translate.get(["YES", "NO", "REPORT.UNKNOWN", "REPORT.REPORT_?","REPORT.DO_NOT_EDIT", "REPORT.ERROR"]).subscribe(translations => {
       let alert = Alert.create({
         title: translations["REPORT.UNKNOWN"],
         message: translations["REPORT.REPORT_?"],
@@ -26,22 +36,26 @@ export class UserReport {
           {
             text: translations["YES"],
             handler: () => {
-              EmailComposer.isAvailable().then(isAvail => {
-                if (isAvail) {
-                  EmailComposer.open({
-                    to: 'methoappeei@gmail.com',
-                    subject: translations['REPORT.TITLE'],
-                    body: translations['REPORT.DO_NOT_EDIT'] + Device.device.platform + " " + Device.device.version + "<br>" + Device.device.model + "<br>" + (window.screen.width * window.devicePixelRatio) + "x" + (window.screen.height * window.devicePixelRatio) + "<br>Cordova " + Device.device.cordova + "</p><br>" + err,
-                    isHtml: true
-                  });
-                }
-              });
+              SocialSharing.shareViaEmail(
+                `${translations['REPORT.DO_NOT_EDIT']}
+                ${Device.device.platform} ${Device.device.version}<br>
+                ${Device.device.manufacturer} ${Device.device.model}<br>
+                ${window.screen.width * window.devicePixelRatio}x${window.screen.height * window.devicePixelRatio}<br>
+                Cordova ${Device.device.cordova}</p><br>
+                ${errStr}<br>
+                ${stacktrace}`,
+                translations['REPORT.ERROR'],
+                ['methoappeei@gmail.com'],
+                [],
+                [],
+                []
+              );
             }
           }
         ]
       });
 
-      this.nav.present(alert);
+      this.app.getActiveNav().present(alert);
     });
   }
 }
